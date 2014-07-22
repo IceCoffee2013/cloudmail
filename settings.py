4d6a8f4261619ca154f86f9d4fc74f170587f605
295046974@qq.com <295046974@qq.com>
2014-07-22 23:40:21 +0800

    init

--- settings.py
+++ settings.py
@@ -81,9 +81,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# TEMPLATE_DIRS = (
+#     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+#     # Always use forward slashes, even on Windows.
+#     # Don't forget to use absolute paths, not relative paths.
+#     '/Users/langley/PycharmProjects/cloudmail/cloudmail/templates',
+# )
+
+
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    '/Users/langley/PycharmProjects/cloudmail/cloudmail/templates',
+	os.path.join(os.path.dirname(__file__), 'templates/'),
 )
