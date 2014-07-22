6305c5befd07f2aeb2b56212c609c0b975bcea24
295046974@qq.com <295046974@qq.com>
2014-07-22 23:31:43 +0800

    init

--- settings.py
+++ settings.py
@@ -47,9 +47,9 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'cloudmail.urls'
+ROOT_URLCONF = 'urls'
 
-WSGI_APPLICATION = 'cloudmail.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 
 # Database
